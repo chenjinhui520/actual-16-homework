da0f06c0c9d819ead4ce4389020c368b4ae09287
diysa <183854527@qq.com>
2017-09-14 17:58:18 +0800

    game py

---
+++ lesson1/yangjianjiang/game.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+import random
+life = 4 #四条命
+num = 1 #初始关数
+money = 0 #初始金币数0
+while num <= 8:
+	print '你目前处于第%s关，你还有%s条命，你一共有%s金币' %(num,life,money)
+	if life == 0:
+		break
+	choice = int(random.random()*10)
+	if choice < 5:
+		money += 10
+		print "你好，我的朋友，给你10个金币，继续下一关吧！"
+	else:
+		life -=1
+		print "去死吧！！贪婪的人类"
+	num += 1
+if life > 0:
+	print "恭喜你闯过所有关卡！"
+else:
+	print "你已经没命了，Game Over!!!!!"

7d83df854fb93ae0d489145d5f2a5c992646c53a
vip@zhaohongye.com <vip@zhaohongye.com>
2017-10-13 13:12:44 +0800

    lesson3 homework zhy

--- lesson3/zhaohongye/homework-02.py
+++ lesson3/zhaohongye/homework-02.py
@@ -10,4 +10,57 @@
 2. 统计一个日志文件中不同IP出现的次数；
 3. 统计同一个ip访问不同一个url的次数；
 '''
+def status_count():
+    File = open('./nginx_access.log')
+    data = File.readlines()
+    File.close()
+    status_dict = {}
+    for line in data:
+        if status_dict.has_key(line.split(' ')[8]) == False :
+            status_dict[line.split(' ')[8]] = 1
+        else:
+            status_dict[line.split(' ')[8]] += 1
+    print '状态码的字典如下：'
+    print status_dict
 
+def ip1_count():
+    File = open('./nginx_access.log')
+    data = File.readlines()
+    File.close()
+    ip1_dict = {}
+    for line in data:
+        if ip1_dict.has_key(line.split(' ')[0]) == False :
+            ip1_dict[line.split(' ')[0]] = 1
+        else:
+            ip1_dict[line.split(' ')[0]] += 1
+    print 'IP的字典如下：'
+    print ip1_dict
+
+
+
+'''
+# 方式1
+{
+    '80.82.78.38 http://www.baidu.com/cache/global/img/gs.gif' : 2,
+    '80.82.78.38 http://www.qq.com/404/search_children.js' : 5,
+    '80.82.78.100 http://www.baidu.com/cache/global/img/gs.gif' : 20,
+}
+'''
+
+def ip_count():
+    File = open('./nginx_access.log')
+    data = File.readlines()
+    File.close()
+    ip_dict = {}
+    for line in data:
+        if ip_dict.has_key(line.split(' ')[0] + ' ' + line.split(' ')[6])  == False:
+            ip_dict[line.split(' ')[0] + ' ' + line.split(' ')[6]] = 1
+        else:
+            ip_dict[line.split(' ')[0] + ' ' + line.split(' ')[6]] += 1
+    print 'IP + 状态码 的字典如下：'
+    print ip_dict
+
+if __name__ == '__main__':
+    status_count()
+    ip1_count()
+    ip_count()

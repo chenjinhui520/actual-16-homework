55e3edff2702d67422c58cbd7539b11d41398073
marshall.zhang <marshall.zhang>
2017-10-13 16:06:08 +0800

    modify login.py

--- lesson3/marshallzhang/login.py
+++ lesson3/marshallzhang/login.py
@@ -3,15 +3,16 @@
 users = {}
 wrong_time = 3
 def regist():
-    user_name = raw_input("请输入注册用户名")
-    passwd = raw_input("请输入注册密码")
+    user_name = raw_input("请输入注册用户名:")
+    passwd = raw_input("请输入注册密码:")
     users.update({user_name:passwd})
 
 def login(wrong_time):
-    user_name = raw_input("请输入登录用户名")
-    passwd = raw_input("请输入登录密码")
+    user_name = raw_input("请输入登录用户名:")
+    passwd = raw_input("请输入登录密码:")
     if  users.get(user_name)==passwd:
         print "登录成功"
+        return(1)
     else:
         print("登录失败 您还有%d次登录机会"%(wrong_time-1))
         wrong_time-=1
@@ -21,8 +22,10 @@ while True:
     if action == "1":
         regist()
     if action == "2":
-        login(wrong_time)
-        wrong_time-=1
+        if login(wrong_time) == 1:
+            break
+        else:
+            wrong_time-=1
     if wrong_time < 1 :
         print "失败次数过多，禁止登录"
         break

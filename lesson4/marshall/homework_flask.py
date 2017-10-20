19bb0b47deb91c17edaa5fd21a10029334ef57d4
marshall.zhang <marshall.zhang>
2017-10-20 16:31:34 +0800

    flask web

--- lesson4/marshall/homework_flask.py
+++ lesson4/marshall/homework_flask.py
@@ -1,13 +1,43 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 import json
 app = Flask(__name__)
-
 @app.route('/')
 def index():
+    return render_template("index.html")
+@app.route('/userlist',methods=['GET','POST'])
+def userlist():
     with open('store.db') as f:
         users=json.loads(f.read())
-        return render_template("index.html",users=users)
+    return render_template("user_list.html",users=users)
+@app.route('/adduser')
+def adduser():
+    return render_template("add_user.html")
+
+@app.route('/login',methods=['POST'])
+
+def login():
+    user = request.form.get('user')
+    passwd = request.form.get('passwd')
+    with open('store.db') as f:
+        users=json.loads(f.read())
+        if user in users and passwd == users[user]:
+            return "succ"
+        else:
+            return redirect('/')
+@app.route('/confirmadd',methods=['POST'])
+def confirmadd():
+    user = request.form.get('user')
+    passwd = request.form.get('passwd')
+    with open('store.db','r') as f:
+        users = json.loads(f.read())
+    with open('store.db','w') as f:
+        users[user] =  passwd
+        users = json.dumps(users)
+        f.write(users)
+    return redirect('userlist')
+
+
+
+
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=9092,debug=True)
-
-
